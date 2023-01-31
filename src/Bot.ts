7c83d1551576bbdabdbf2556c48efb7c4916e9c0
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2023-01-31 15:36:37 -0600

    Remove repeated code

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,8 +2,7 @@ const { Client, ClientOptions, REST, SlashCommandBuilder, Routes, ChatInputComma
 import * as dotenv from 'dotenv';
 import ready from './Ready';
 import { commands } from './Commands';
-import { getEventsInfo, getMeetingsInfo, getTodoInfo, getMarketingInfo, formatInfo } from './Util';
-import { table } from 'table';
+import { sheetsList, getSheetsInfo, formatInfo, sendMessage } from './Util';
 
 
 dotenv.config();
@@ -26,38 +25,34 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data: any) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
 
-const table_config = {
-    singleLine: true,
-};
-
 client.on('interactionCreate', async (interaction: any) => {
 	if (!interaction.isChatInputCommand()) return;
 
         const { commandName } = interaction;
-        let res: any;
+        let res: string[][];
         switch(commandName){
             case 'hello':
                 await interaction.reply("Hello, world!");
                 break;
             case 'geteventsinfo':
                 await interaction.deferReply();
-                res = await getEventsInfo();
-                await interaction.editReply(res?.[0]?.[0]);
+                res = await getSheetsInfo(sheetsList[0]);
+                await sendMessage(interaction, res);
                 break;
             case 'getmeetingsinfo':
                 await interaction.deferReply();
-                res = await getMeetingsInfo();
-                await interaction.editReply(res?.[0]?.[0]);
+                res = await getSheetsInfo(sheetsList[1]);
+                await sendMessage(interaction, res);
                 break;
             case 'gettodoinfo':
                 await interaction.deferReply();
-                res = await getTodoInfo();
-                await interaction.editReply(table(formatInfo((await getTodoInfo()).slice(0, 4))), table_config);
+                res = await getSheetsInfo(sheetsList[2]);
+                await sendMessage(interaction, res);
                 break;
             case 'getmarketinginfo':
                 await interaction.deferReply();
-                res = await getMarketingInfo();
-                await interaction.editReply(res?.[0]?.[0]);
+                res = await getSheetsInfo(sheetsList[3]);
+                await sendMessage(interaction, res);
                 break;
         }
 });
--- src/Util.ts
+++ src/Util.ts
@@ -1,81 +1,34 @@
 import {google} from 'googleapis';
+import { table } from 'table';
+
 
 const googleApiAuth = process.env.GOOGLE_API_KEY
 const googleSpreadsheetID = process.env.GOOGLE_SPREADSHEET_ID
 
+const sheets = google.sheets({
+    version: 'v4',
+    auth: googleApiAuth,
+});
+
+// List of sheets in the planner
+export const sheetsList = ['Events 📍', 'Meetings 📆', 'Todo 📋', 'Marketing 📷'];
+
 // Get info from planner
-
-export const getEventsInfo = async () => {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: googleApiAuth,
-    });
-    
+export const getSheetsInfo = async (sheetName: string): Promise<string[][]> => {
     const params = {
         spreadsheetId: googleSpreadsheetID,
-        range: 'Events 📍',
+        range: sheetName,
         valueRenderOption: 'FORMATTED_VALUE',
         dateTimeRenderOption: 'FORMATTED_STRING',
         majorDimension: 'ROWS',
     };
-    const res = await sheets.spreadsheets.values.get(params);
-    return res.data.values;
-}
 
-export const getMeetingsInfo = async () => {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: googleApiAuth,
-    });
-    
-    const params = {
-        spreadsheetId: googleSpreadsheetID,
-        range: 'Meetings 📆',
-        valueRenderOption: 'FORMATTED_VALUE',
-        dateTimeRenderOption: 'FORMATTED_STRING',
-        majorDimension: 'ROWS',
-    };
-    const res = await sheets.spreadsheets.values.get(params);
-    return res.data.values;
-}
-
-export const getTodoInfo = async (): Promise<string[][]> => {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: googleApiAuth,
-    });
-    
-    const params = {
-        spreadsheetId: googleSpreadsheetID,
-        range: 'Todo 📋',
-        valueRenderOption: 'FORMATTED_VALUE',
-        dateTimeRenderOption: 'FORMATTED_STRING',
-        majorDimension: 'ROWS',
-    };
     const res: any = await sheets.spreadsheets.values.get(params);
     return res?.data.values;
 }
 
-export const getMarketingInfo = async () => {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: googleApiAuth,
-    });
-    
-    const params = {
-        spreadsheetId: googleSpreadsheetID,
-        range: 'Marketing 📷',
-        valueRenderOption: 'FORMATTED_VALUE',
-        dateTimeRenderOption: 'FORMATTED_STRING',
-        majorDimension: 'ROWS',
-    };
-    const res = await sheets.spreadsheets.values.get(params);
-    return res.data.values;
-}
-
-
 // Format info for the table function in Bot.ts
-export const formatInfo = (info: any) => {
+export const formatInfo = (info: any): string[][] => {
     let numElements = info?.[0].length;
 
     for (let row of info) {
@@ -88,4 +41,14 @@ export const formatInfo = (info: any) => {
     }
 
     return info;
+}
+
+// Handle interaction and send formatted message
+export const sendMessage = async (interaction: any, res: string[][]) => {
+    
+    const table_config = {
+        singleLine: true,
+    };
+
+    await interaction.editReply(table(formatInfo(res.slice(0, 4))), table_config);
 }
